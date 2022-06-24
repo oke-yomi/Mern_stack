e395a046a1b255e3d8685bc54ff1d4e4b853462c
oke-yomi <okeoluwayomi9@gmail.com>
2022-06-24 05:43:44 +0100

    chnages

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
+
 const workoutRoutes = require("./routes/workouts");
 
 // express app
@@ -18,7 +20,15 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRoutes);
 
-// listen for requests
-app.listen(PORT, () => {
-	console.log(`listening on port ${PORT}`);
-});
+// connect to db
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		// listen for requests
+		app.listen(PORT, () => {
+			console.log(`connected to db & listening on port ${PORT}`);
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
