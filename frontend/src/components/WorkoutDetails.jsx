d4fcb8dc9f7c0ff9a53936351322103933ed492e
oke-yomi <okeoluwayomi9@gmail.com>
2022-07-03 17:31:20 +0100

    handle delete

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
 
 const WorkoutDetails = ({ workout }) => {
+	const { dispatch } = useWorkoutContext();
+
+	const handleDelete = async () => {
+		const response = await fetch("/api/workouts/" + workout._id, {
+			method: "DELETE",
+		});
+		const json = await response.json();
+
+		if (response.ok) {
+			dispatch({ type: "DELETE_WORKOUT", payload: json });
+		}
+	};
+
 	return (
 		<div className="workout-details">
 			<h4>{workout.title}</h4>
@@ -13,6 +27,7 @@ const WorkoutDetails = ({ workout }) => {
 				{workout.reps}
 			</p>
 			<p>{workout.createdAt}</p>
+			<span onClick={handleDelete}>🗑️</span>
 		</div>
 	);
 };
